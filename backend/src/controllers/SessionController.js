90636bec77d4f78dcab73deeb6ea2cb321e9515d
cassielrattes <cassielrattes@gmail.com>
2019-10-05 22:19:48 -0300

    First Commit

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,19 @@
+// index, show, store, update, destroy
+const User = require('../models/User');
+
+
+module.exports = {
+    async store(req,res){
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if(!user){
+            user = await User.create({ email });
+        }
+
+        // const user = await User.create({ email });
+
+        return res.json(user);
+    }
+};
---
+++ backend/src/models/Spot.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const SpotSchema = new mongoose.Schema({
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }
+}, {
+    toJSON: {
+        virtuals: true,                                                                                                                    
+    },
+});
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://192.168.15.17:3000/files/${this.thumbnail}`
+})
+
+module.exports = mongoose.model('Spot', SpotSchema);                                                                                                                                                                                                                                                                                                                                                                                                                                        
